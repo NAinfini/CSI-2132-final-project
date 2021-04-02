f2c173440c4dab6ba24920fc3828ffaabb990df0
Daniel Sui <NAinfini@users.noreply.github.com>
2021-04-02 12:11:00 -0400

    login process added

--- src/mainProgram.java
+++ src/mainProgram.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
 
 public class mainProgram {
-	
+	private boolean loginDone = false;
 	 public static void main(String args[]) {
 		 mainProgram program = new mainProgram();
+		 program.loginProcess();
+		 
+
+	 }
+	 private void loginProcess() {
 		 Scanner myObj = new Scanner(System.in);
-		 System.out.print("Hello, welcome to the hotel thingy"); 
-		 System.out.print("please enter your username, or regiter to register a new account"); 
-		 String userInput = myObj.nextLine();
-		 while(!userInput.equals("register") && !program.validateUserName(userInput)) {
-			 System.out.print("invalid username, please try again"); 
-			 userInput = myObj.nextLine();
-		 }
-		 if(userInput.equals("register")) {
-			 program.registerUser();
-		 }else {
-			 if(program.getUserType(userInput).equals("customer")) {
-				 customerPage customer= new customerPage();
-			 }else if(program.getUserType(userInput).equals("employee")) {
-				 employeePage employee = new employeePage();
+		 System.out.print("Hello, welcome to the hotel thingy.\n"); 
+		 while(!loginDone) {
+			 System.out.print("please enter your username, or type \"regiter\" to register a new account.\n"); 
+			 String userInput = myObj.nextLine();
+			 while(!userInput.equals("register") && !validateUserName(userInput)) {
+				 System.out.print("invalid username, please try again\n"); 
+				 userInput = myObj.nextLine();
+			 }
+			 if(userInput.equals("register")) {
+				 registerUser();
 			 }else {
-				 System.out.print("Your type doesnt exist in database, something went wrong"); 
+				 if(getUserType(userInput).equals("customer")) {
+					 loginDone = true;
+					 customerPage customer= new customerPage();
+				 }else if(getUserType(userInput).equals("employee")) {
+					 loginDone = true;
+					 employeePage employee = new employeePage();
+				 }else {
+					 System.out.print("Your type doesnt exist in database, something went wrong"); 
+				 }
 			 }
 		 }
 	 }
-	 
 	 private boolean validateUserName(String userName) {
 		 return accessDataBase.getInstance().validateUserName(userName);
 	 }
@@ -32,6 +40,10 @@ public class mainProgram {
 		 return accessDataBase.getInstance().getUserType(userInput);
 	 }
 	 private boolean registerUser() {
+		 Scanner myObj = new Scanner(System.in);
+		 System.out.print("enter username here\n"); 
+		 String userInput = myObj.nextLine();
+		 System.out.print(userInput + " is now the username\n"); 
 		 return true;
 	 }
 	 
