fbdec391a10af390a1ebe5806baf7a28edf72f7c
LeithGrira <lgrira2000@gmail.com>
2021-04-03 01:09:24 -0400

    adding checks for the booking id

--- src/employeePage.java
+++ src/employeePage.java
@@ -8,34 +8,82 @@ public class employeePage {
 	
 	employeePage(){
 		String userInput;
+		Boolean customerValid = false;
 		System.out.print("\nHello, you logged in as a employee\n\n"); 
-		System.out.print("What is the Booking ID of this customer?\n");
-		System.out.print("Booking ID number: ");
-		Scanner myObj = new Scanner(System.in);
 		
+		while(!customerValid) {
+			System.out.print("What is the Booking ID of this customer?\n");
+			System.out.print("Booking ID number: ");
+			
+			Scanner myObj = new Scanner(System.in);
+			this.checkBooking(myObj);
+			
+			System.out.print("This customer is Mr/Mrs "+this.first_name+" "+ this.last_name+ ", Do you confirm their identity? (yes/no)\n");
+			
+			Boolean response = true;
+			while(response) {
+				userInput = myObj.nextLine().trim();
+				switch(userInput) {
+				case "yes":
+					response = false;
+					customerValid = true;
+					break;
+				case "no":
+					System.out.print("\nPlease make sure that the customer has a booking\n");
+					response = false;
+					customerValid = false;
+					break;
+				default:
+					System.out.print("Invalid response, please try again!\n");
+				}
+			}
+		}
+		
+		
+		
+		System.out.print("\nloop ended\n");
+		
+		
+	}
+	
+	/*
+	 * Update the first name and last name of the customer 
+	 */
+	private void updateFullName(int booking_id) {
+		String[] result = accessDataBase.getInstance().getFullName(booking_id);
+		this.first_name = result[0];
+		this.last_name = result[1];
+	 }
+	
+	/*
+	 * Check if the booking is valid  
+	 */
+	private void checkBooking(Scanner myObj) {
+		String userInput;
 		while(true) {
 			userInput = myObj.nextLine().trim();
+			
+			// Check if the input is a valid Integer
 			if(isInteger(userInput)) {
 				this.Booking_ID = Integer.parseInt(userInput);
-				break;
+				this.updateFullName(this.Booking_ID);
+				
+				// If the booking ID doesn't exist, ask the customer to retype the booking ID
+				// else, the booking is valid
+				if (this.first_name == null || this.last_name == null) {
+					System.out.print("\nPlease enter a valid Booking ID!\n");
+					System.out.print("Booking ID number: ");
+				} else {
+					break;
+				}
+				
 			} else {
 				System.out.print("\nPlease enter a valid number!\n");
 				System.out.print("Booking ID number: ");
 			}
 			
 		}
-		
-		this.updateFullName(this.Booking_ID);
-		System.out.print("This customer is Mr/Mrs "+this.first_name+" "+ this.last_name+ ", Do you confirm their identity? (yes/no)\n");
-		
-		
 	}
-	
-	private void updateFullName(int booking_id) {
-		String[] result = accessDataBase.getInstance().getFullName(booking_id);
-		this.first_name = result[0];
-		this.last_name = result[1];
-	 }
 	 
 
 	/*
